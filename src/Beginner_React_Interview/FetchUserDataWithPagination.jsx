f4c2bf6517b4b634a1cbb6b9192c4e7d0f3d546a
belghiti.brahim <belghiti.brahim@protonmail.com>
2022-11-09 10:33:38 +0100

    done

--- src/Beginner_React_Interview/FetchUserDataWithPagination.jsx
+++ src/Beginner_React_Interview/FetchUserDataWithPagination.jsx
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from 'react';
 
 export default function FetchingDataFromAnApi() {
-  const [pageNumber, setPageNumber] = useState(5);
+  const [pageNumber, setPageNumber] = useState(1);
   const API_URL = `https://api.randomuser.me/?page=${pageNumber}`;
   const [userInformation, setUserInformation] = useState([]);
 
   const fetchNextUser = () => {
     setPageNumber(pageNumber + 1);
-     
   };
 
   async function fetchApiData() {
@@ -15,49 +14,42 @@ export default function FetchingDataFromAnApi() {
       let response = await fetch(API_URL);
       let data = await response.json();
       const { results, info } = data;
-      const randomUserInfomation = results.map((item) => ({
-        randomUserName: item.name.first + ' ' + item.name.last,
-        randomUserImage: item.picture.large,
-      }));
+      const [result] = results;
+      const userName = `${result.name.first} ${result.name.last}`;
+      const userImage = result.picture.large;
+      const randomUserInfomation = { userName, userImage };
       return randomUserInfomation;
     } catch (err) {
-      console.eroor(err.message);
+      console.error(err.message);
     }
   }
 
   useEffect(() => {
     fetchApiData().then((randomUserInfomation) => {
-      let dataToUpdate = {};
-      randomUserInfomation.map((item) => {
-        dataToUpdate = {
-          userName: item.randomUserName,
-          userImage: item.randomUserImage,
-        };
-        return dataToUpdate;
-      });
-      setUserInformation((userInformation) => ({
+
+      const updatedUserInfos = [
         ...userInformation,
-        ...dataToUpdate,
-      }));
-      // setUserInformation(dataToUpdate);
+        randomUserInfomation,
+      ]
+      setUserInformation(updatedUserInfos)
     });
   }, [pageNumber]);
 
   return (
     <>
-      <div className="userCards">
-        {userInformation.length > 0 ? (
-          userInformation.map((item, i) => (
-            <div key={i} className="userCard">
-              <img src={item.userImage} alt="user" />
-              <p>{item.userName}</p>
-            </div>
-          ))
-        ) : (
-          <p>no data to display</p>
-        )}
-      </div>
-      <button onClick={fetchNextUser}>add new user</button>
+        <div className="userCards">
+          {userInformation.length > 0 ? (
+            userInformation.map((item, i) => (
+              <div key={i + 1} className="userCard">
+                <img src={item.userImage} alt="user" />
+                <p>{item.userName}</p>
+              </div>
+            ))
+          ) : (
+            <p>no data to display</p>
+          )}
+        </div>
+        <button onClick={fetchNextUser}>add new user</button>
     </>
   );
 }
